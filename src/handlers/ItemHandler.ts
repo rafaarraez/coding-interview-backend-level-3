1498a8bda53643e0b87b79c6e738114ab10c3006
Rafael <rafa.arraez.gue@gmail.com>
2025-02-03 19:18:57 +0000

    add trycatch to handlers functions

--- src/handlers/ItemHandler.ts
+++ src/handlers/ItemHandler.ts
@@ -7,45 +7,66 @@ export class ItemHandlers {
             const items = await ItemService.getAllItems();
             return h.response(items).code(200);
         } catch (error) {
-            console.log(error);
+            console.error('Error in getAllItems:', error);
+            return h.response({ message: 'Internal Server Error' }).code(500);
         }
     }
 
     static async getItemById(request: Request, h: ResponseToolkit) {
-        const id = parseInt(request.params.id, 10);
-        const item = await ItemService.getItemById(id);
+        try {
+            const id = parseInt(request.params.id, 10);
+            const item = await ItemService.getItemById(id);
 
-        if (!item) return h.response({ message: 'Item not found' }).code(404);
+            if (!item) return h.response({ message: 'Item not found' }).code(404);
 
-        return h.response(item).code(200);
+            return h.response(item).code(200);
+        } catch (error) {
+            console.error('Error in getItemById:', error);
+            return h.response({ message: 'Internal Server Error' }).code(500);
+        }
     }
 
     static async createItem(request: Request, h: ResponseToolkit) {
-        const { name, price } = request.payload as { name: string; price: number };
+        try {
+            const { name, price } = request.payload as { name: string; price: number };
 
-        if (price < 0) return h.response({ errors: [{ field: 'price', message: 'Field "price" cannot be negative' }] }).code(400);
-        if (!name || price === undefined) return h.response({ errors: [{ field: 'price', message: 'Field "price" is required' }] }).code(400);
+            if (price < 0) return h.response({ errors: [{ field: 'price', message: 'Field "price" cannot be negative' }] }).code(400);
+            if (!name || price === undefined) return h.response({ errors: [{ field: 'price', message: 'Field "price" is required' }] }).code(400);
 
-        const item = await ItemService.createItem(name, price);
-        return h.response(item).code(201);
+            const item = await ItemService.createItem(name, price);
+            return h.response(item).code(201);
+        } catch (error) {
+            console.log('Error trying to createItem: ', error)
+            return h.response({ message: 'Internal Server Error' }).code(500);
+        }
     }
 
     static async updateItem(request: Request, h: ResponseToolkit) {
-        const id = parseInt(request.params.id, 10);
-        const { name, price } = request.payload as { name: string; price: number };
-        if (price < 0) {
-            return h.response({ errors: [{ field: 'price', message: 'Field "price" cannot be negative' }] }).code(400);
+        try {
+            const id = parseInt(request.params.id, 10);
+            const { name, price } = request.payload as { name: string; price: number };
+            if (price < 0) {
+                return h.response({ errors: [{ field: 'price', message: 'Field "price" cannot be negative' }] }).code(400);
+            }
+            const item = await ItemService.updateItem(id, name, price);
+            if (item) {
+                return h.response(item).code(200);
+            }
+            return h.response({ message: 'Item not found' }).code(404);
+        } catch (error) {
+            console.log('Error in updateItem: ', error);
+            return h.response({ message: 'Internal Server Error' }).code(500);
         }
-        const item = await ItemService.updateItem(id, name, price);
-        if (item) {
-            return h.response(item).code(200);
-        }
-        return h.response({ message: 'Item not found' }).code(404);
     }
 
     static async deleteItem(request: Request, h: ResponseToolkit) {
-        const id = parseInt(request.params.id, 10);
-        await ItemService.deleteItem(id);
-        return h.response().code(204);
+        try {
+            const id = parseInt(request.params.id, 10);
+            await ItemService.deleteItem(id);
+            return h.response().code(204);
+        } catch (error) {
+            console.log('Error in deleteItem: ', error);
+            return h.response({ message: 'Internal Server Error' }).code(500);
+        }
     }
 }
